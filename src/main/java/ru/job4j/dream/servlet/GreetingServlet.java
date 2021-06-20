5ca7bbf97b1833cb3d9583136ecdbfa021432e04
AlexKir008 <kirshaav@yandex.ru>
2021-06-20 21:43:39 +0300

    4.1. JSON формат. [#238554]

--- src/main/java/ru/job4j/dream/servlet/GreetingServlet.java
+++ src/main/java/ru/job4j/dream/servlet/GreetingServlet.java
@@ -4,19 +4,40 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.stream.Collectors;
+
+import com.google.gson.Gson;
+import com.google.gson.JsonObject;
+import org.json.JSONObject;
 
 public class GreetingServlet extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.setHeader("Access-Control-Allow-Origin", "*");
         resp.setContentType("text/plain");
         resp.setCharacterEncoding("UTF-8");
         String name = req.getParameter("name");
-        resp.setHeader("Access-Control-Allow-Origin", "*");
         PrintWriter writer = new PrintWriter(resp.getOutputStream());
         writer.println("Nice to meet you, " + name);
         writer.flush();
     }
+
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.setHeader("Access-Control-Allow-Origin", "*");
+        JsonObject data = new Gson().fromJson(req.getReader(), JsonObject.class);
+        String text = data.get("text").getAsString();
+        resp.setCharacterEncoding("UTF-8");
+        resp.setContentType("text/json");
+        PrintWriter writer = new PrintWriter(resp.getOutputStream());
+        String line = "Nice to meet you, " + text;
+        JSONObject outLineJson = new JSONObject();
+        outLineJson.put("text", line);
+        writer.println(outLineJson);
+        writer.flush();
+    }
+
 }
--- src/main/webapp/index.jsp
+++ src/main/webapp/index.jsp
@@ -36,6 +36,9 @@
             <li class="nav-item">
                 <a class="nav-link" href="<%=request.getContextPath()%>/upload.jsp">Загр на серв</a>
             </li>
+            <li class="nav-item">
+                <a class="nav-link" href="<%=request.getContextPath()%>/AjaxJson.html">Ajax</a>
+            </li>
             <%--<li class="nav-item">
                 <a class="nav-link" href="<%=request.getContextPath()%>/download">Загр</a>
             </li>--%>
